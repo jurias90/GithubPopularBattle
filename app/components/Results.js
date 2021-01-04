0cde59de2a671f98690c894b9870caf7608f0c28
Jesus Urias <jesusurias@jesuss-mbp.attlocal.net>
2021-01-03 21:49:37 -0800

    Added Copy Webpack Plugin to allow Netlify to use React Routing properly

--- app/components/Results.js
+++ app/components/Results.js
@@ -12,8 +12,8 @@ import Card from "./Card";
 import PropTypes from "prop-types";
 import Loading from "./Loading";
 import Tooltip from "./Tooltip";
-import queryString from 'query-string'
-import { Link } from 'react-router-dom'
+import queryString from "query-string";
+import { Link } from "react-router-dom";
 
 const styles = {
 	container: {
@@ -77,23 +77,27 @@ ProfileList.propTypes = {
 };
 
 export default class Results extends React.Component {
-
 	state = {
 		winner: null,
 		loser: null,
 		error: null,
-		loading: true
-	}
+		loading: true,
+	};
 	componentDidMount() {
-		const { playerOne, playerTwo } = queryString.parse(this.props.location.search)
+		const { playerOne, playerTwo } = queryString.parse(
+			this.props.location.search
+		);
 
-		battle([playerOne, playerTwo]).then((players) => {
-			this.setState({
-				winner: players[0],
-				loser: players[1],
-				error: null,
-				loading: false,
-			})}).catch((message) => {
+		battle([playerOne, playerTwo])
+			.then((players) => {
+				this.setState({
+					winner: players[0],
+					loser: players[1],
+					error: null,
+					loading: false,
+				});
+			})
+			.catch((message) => {
 				this.setState({
 					error: message,
 					loading: false,
@@ -134,7 +138,7 @@ export default class Results extends React.Component {
 						<ProfileList profile={loser.profile} />
 					</Card>
 				</div>
-				<Link className="btn btn-drk btn-space" to='/battle'>
+				<Link className="btn btn-drk btn-space" to="/battle">
 					Reset
 				</Link>
 			</React.Fragment>
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: "./app/index.js",
-    output: {
-        path:path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js',
-        publicPath : '/'
-    },
-    module:{
-        rules:[
-            {test: /\.(js)$/, use: 'babel-loader'},
-            {test: /\.css$/, use: [ 'style-loader' ,'css-loader']}
-        ]
-    },
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: 'app/index.html'
-        })
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-        historyApiFallback: true
-      }
-
+	entry: "./app/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index_bundle.js",
+		publicPath: "/",
+	},
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: "babel-loader" },
+			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
+		],
+	},
+	mode: process.env.NODE_ENV === "production" ? "production" : "development",
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "app/index.html",
+		}),
+		new CopyPlugin({ patterns: [{ from: "_redirects" }] }),
+	],
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 9000,
+		historyApiFallback: true,
+	},
 };
